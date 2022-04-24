8da0b67e5bfbbeb0ce69f0cace7b5a8428a9e7bb
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-04-24 15:47:03 +0200

    Add list component

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useState, useEffect, Fragment } from "react";
 
 const List = () => {
-  return <div>List</div>;
+  const [article, setArticle] = useState([]);
+
+  const addArticle = (e) => {
+    setArticle(e.target.value);
+  };
+
+  return (
+    <Fragment>
+      <div className="flex flex-col items-center min-h-screen w-full p-10">
+        <form className="flex justify-center align-middle p-2.5 m-10 animate-titleAppear">
+          <input
+            onChange={addArticle}
+            type="text"
+            value={article}
+            className="border-2 border-solid h-10 pl-2"
+            placeholder="Add a product"
+          />
+        </form>
+      </div>
+    </Fragment>
+  );
 };
 
 export default List;
