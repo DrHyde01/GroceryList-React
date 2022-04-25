03940f5f98a8a9235dc1593bccd67dcb05d3c433
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-04-25 22:19:09 +0200

    Add list logic

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -2,23 +2,42 @@ import React from "react";
 import { useState, useEffect, Fragment } from "react";
 
 const List = () => {
-  const [article, setArticle] = useState([]);
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
 
-  const addArticle = (e) => {
-    setArticle(e.target.value);
+  //Add item from the input
+  const handleChange = (e) => {
+    setItem(e.target.value);
   };
+
+  // And move item to the array
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setList([...list, item]);
+
+    setItem("");
+  };
+
+  // Add updated list to LocalStorage
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
   return (
     <Fragment>
-      <div className="flex flex-col items-center min-h-screen w-full p-10">
-        <form className="flex justify-center align-middle p-2.5 m-10 animate-titleAppear">
+      <div className="flex flex-col items-center min-h-screen w-full py-10">
+        <form
+          onSubmit={handleSubmit}
+          className="flex justify-center align-middle w-full p-2.5 m-10 animate-titleAppear"
+        >
           <input
-            onChange={addArticle}
+            onChange={handleChange}
             type="text"
-            value={article}
-            className="border-2 border-solid h-10 pl-2"
+            value={item}
+            className="border-2 border-solid w-1/3 h-10 pl-2"
             placeholder="Add a product"
           />
+          <button className="flex justify-center items-center ml-3">Add</button>
         </form>
       </div>
     </Fragment>
