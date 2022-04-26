69b0a0039713751dd1f69296c1a01885a503e1ef
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-04-26 16:32:56 +0200

    Make more responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       className="flex justify-center align-center w-full h-auto p-14 bg-customGreen bg-fixed "
       style={{ backgroundImage: `url('${background}')` }}
     >
-      <div className="flex flex-col justify-center w-1/3 p-5 bg-customYellow shadow-[_5px_5px_15px_#000]  animate-titleAppear">
+      <div className="flex flex-col justify-center xl:w-1/3 2xl:w-1/4 p-5 bg-customYellow shadow-[_5px_5px_15px_#000]  animate-titleAppear">
         <h1 className="text-4xl font-semibold text-center p-4">
           My Grocery List
         </h1>
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -48,26 +48,26 @@ const List = () => {
 
   return (
     <Fragment>
-      <div className="flex flex-col items-center min-h-screen w-full py-10 bg-gray-50">
+      <div className="flex flex-col items-center min-h-screen w-full px-6 md:px-20 lg:px-0 py-10 bg-gray-50">
         <form
           onSubmit={handleSubmit}
-          className="flex justify-center  items-center w-full p-2.5 m-10 animate-titleAppear"
+          className="flex flex-col md:flex-row justify-center items-center w-full p-2.5 lg:m-10 animate-titleAppear"
         >
           <input
             onChange={handleChange}
             type="text"
             value={item}
-            className="border-2 border-solid border-gray-400 bg-white w-1/3 h-12 pl-2  focus:outline-none  focus:border-customGreen"
+            className="border-2 border-solid border-gray-400 bg-white w-full  md:w-1/2 lg:w-1/3 2xl:w-1/4 h-12 pl-2  focus:outline-none  focus:border-customGreen"
             placeholder="Add a product"
           />
-          <button className="flex justify-center items-center ml-4 p-3 rounded-full bg-customGreen hover:scale-110 hover:shadow-2xl transition delay-150 duration-300 ease-in-out">
+          <button className="flex justify-center items-center md:ml-4 mt-8 md:mt-0 p-3 rounded-full bg-customGreen hover:scale-110 hover:shadow-2xl transition delay-150 duration-300 ease-in-out">
             <AiOutlinePlus size={25} color={"white"} />
           </button>
         </form>
 
         {list.length > 0 ? (
           <Fragment>
-            <div className="flex flex-col justify-start w-2/6 m-10 p-8 bg-customYellow shadow-lg animate-titleAppear">
+            <div className="flex flex-col justify-start w-full md:w-3/4 lg:w-2/6 m-10 p-8 bg-customYellow shadow-lg animate-titleAppear">
               {list.map((a, id) => (
                 <Items
                   key={id}
@@ -86,7 +86,7 @@ const List = () => {
             </button>
           </Fragment>
         ) : (
-          <div className="flex flex-col justify-center items-center p-10 w-2/4">
+          <div className="flex flex-col justify-center items-center py-10 md:w-2/4">
             <p className="text-center text-gray-500 leading-8 mb-8">
               Your list is empty ! 🙁 <br />
               Add a item with the input above 👆🏻
